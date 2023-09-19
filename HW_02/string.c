898be3d08bc0922328ac4ebf857ad3ef101f5229
Sotvictory <sotvictory@gmail.com>
2023-09-19 15:16:16 +0300

    add reverse(s)

--- HW_02/string.c
+++ HW_02/string.c
@@ -12,8 +12,20 @@ int mystrlen(const char *s)
     return i;
 }
 
+/* reverse a string */
+void reverse(char *s)
+{
+    int i = 0, len = mystrlen(s);
+    char tmp;
+
+    for (i = 0; i < len / 2; i++) {
+        tmp = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = tmp;
+    }
+}
+
 /* Написать функцию
-! 2) reverse (s) - переставляет в обратном порядке элементы заданной строки на том же месте памяти
 ! 3) cdelete (s, c) - удаляет из заданной строки s все символы c.
 4) scandigit( s) – определяет количество символов-цифр в заданной строке
 5) scat (s1, s2, s3) – создает из двух, упорядоченных по алфавиту строк s1  и  s2 строку s3, также упорядоченную по алфавиту
@@ -26,6 +38,11 @@ int main(void)
     char str[] = "These are functions for processing strings"; // length 42 not including zero
 
     printf("The length of string is %d\n", mystrlen(str));
+
+    reverse(str);
+    printf("Reversed string: %s\n", str);
+    reverse(str); // return to its original form
+    printf("Double reversed string: %s\n", str);
 
     return 0;
 }
