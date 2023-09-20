5b3943c8e1cec6ee22e44f602cb2865ef2fd0d8f
Sotvictory <sotvictory@gmail.com>
2023-09-20 19:38:36 +0300

    add scalar_prod_matrix()

--- Task-2/main.c
+++ Task-2/main.c
@@ -3,19 +3,20 @@
 
 enum { 
     row_cnt = 2,
-    col_cnt = 3
+    col_cnt = 3,
 };
+
+const double multiplier = 2.0;
 
 int main(void)
 {
-    matrix **mas_1, **mas_2, **mas_sum;
+    matrix **mas_1, **mas_2, **mas_sum, **mas_scalar;
     
     mas_1 = read_matrix(row_cnt, col_cnt);
     mas_2 = read_matrix(row_cnt, col_cnt);
 
     printf("Matrix 1:\n");
     print_matrix(mas_1, row_cnt, col_cnt);
-    printf("\n");
     printf("Matrix 2:\n");
     print_matrix(mas_2, row_cnt, col_cnt);
 
@@ -23,9 +24,14 @@ int main(void)
     printf("Sum of matrix 1 and matrix 2:\n");
     print_matrix(mas_sum, row_cnt, col_cnt);
 
+    mas_scalar = scalar_prod_matrix(multiplier, mas_1, row_cnt, col_cnt);
+    printf("Product of multiplier and matrix 1:\n");
+    print_matrix(mas_scalar, row_cnt, col_cnt);
+
     free_matrix(mas_1, row_cnt);
     free_matrix(mas_2, row_cnt);
     free_matrix(mas_sum, row_cnt);
+    free_matrix(mas_scalar, row_cnt);
 
     return 0;
 }
--- Task-2/matrix.c
+++ Task-2/matrix.c
@@ -53,6 +53,22 @@ matrix **sum_matrix(matrix **mas_1, matrix **mas_2, int row_cnt, int col_cnt)
             mas[i][j] = mas_1[i][j] + mas_2[i][j];
         }
     }
-    
+
     return mas;
+}
+
+matrix **scalar_prod_matrix(double multiplier, matrix **mas, int row_cnt, int col_cnt)
+{
+    matrix **mas_scalar;
+    int i, j;
+
+    mas_scalar = (matrix **) malloc(sizeof(matrix *) * row_cnt);
+    for (i = 0; i < row_cnt; i++) {
+        mas_scalar[i] = (matrix *) malloc(sizeof(matrix) * col_cnt);
+        for (j = 0; j < col_cnt; j++) {
+            mas_scalar[i][j] = multiplier * mas[i][j];
+        }
+    }
+
+    return mas_scalar;
 }
