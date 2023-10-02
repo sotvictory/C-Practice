094b371893a42ec983d45b4569e97e6eb924a557
Sotvictory <sotvictory@gmail.com>
2023-10-02 21:48:25 +0300

    add list_node_delete_all()

--- HW_04/list.c
+++ HW_04/list.c
@@ -49,7 +49,7 @@ void list_insert_end(int number, list lst)
     lst->next = cur;
 }
 
-void list_destruct(list lst)
+list list_destruct(list lst)
 {
     link cur;
 
@@ -58,6 +58,8 @@ void list_destruct(list lst)
         lst = lst->next;
         free(cur);
     }
+
+    return NULL;
 }
 
 list list_node_delete(int number, list lst)
@@ -83,20 +85,50 @@ list list_node_delete(int number, list lst)
     return lst;
 }
 
+list list_node_delete_all(int number, list lst)
+{
+    link prev = NULL, cur = lst, tmp;
+
+    while (cur != NULL) {
+        if (cur->elem == number) {
+            if (prev == NULL) {
+                lst = cur->next;
+            } else {
+                prev->next = cur->next;
+            }
+            tmp = cur;
+            cur = cur->next;
+            free(tmp);
+        } else {
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+
+    return lst;
+}
+
 int main(void)
 {
-    list lst = list_create(5);
+    list lst = NULL;
+    list_print(lst);
+    lst = list_create(3);
+    list_print(lst);
 
     list_insert_end(3, lst);
+    list_insert_end(2, lst);
     list_insert_end(6, lst);
-    list_insert_end(0, lst);
-    list_insert_end(1, lst);
-
+    list_insert_end(3, lst);
+    list_insert_end(2, lst);
+    list_insert_end(3, lst);
     list_print(lst);
-    lst = list_node_delete(3, lst);
+    
+    lst = list_node_delete(2, lst);
+    lst = list_node_delete_all(3, lst);
     list_print(lst);
 
-    list_destruct(lst);
+    lst = list_destruct(lst);
+    list_print(lst);
 
     return 0;
 }
