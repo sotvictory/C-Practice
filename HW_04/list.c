31c298203795b8a0dd79a3786b18beb0070876cd
Sotvictory <sotvictory@gmail.com>
2023-10-02 21:06:13 +0300

    add list_destruct(), list_insert_end(), modify list_print()

--- HW_04/list.c
+++ HW_04/list.c
@@ -2,45 +2,75 @@
 # include <stdlib.h>
 
 typedef struct Node *link;
+typedef link list;
 
 typedef struct Node { 
         int elem;
         link next;
 } node;
 
-typedef link list;
-
 list list_create(int number)
 {
-    link cur;
-    list res;
+    list lst = (link) malloc(sizeof(node));
+    lst->elem = number;
+    lst->next = NULL;
 
-    res = cur = (link) malloc(sizeof(node));
-    cur->elem = number;
-    cur->next = NULL;
-
-    return res;
+    return lst;
 }
 
-void list_print(list p)
+void list_print(list lst)
 {
-    while(p != NULL) {
-        if (p->next != NULL) {
-            printf("%d ", p->elem);
-            p = p->next;
+    if (lst == NULL) {
+        fprintf(stderr, "The list is empty\n");
+        return;
+    }
+
+    while(lst != NULL) {
+        if (lst->next != NULL) {
+            printf("%d ", lst->elem);
+            lst = lst->next;
         } else {
-            printf("%d\n", p->elem);
+            printf("%d\n", lst->elem);
             break;
         }
     }
 }
 
+void list_insert_end(int number, list lst)
+{
+    link cur = (link) malloc(sizeof(node));
+    cur->elem = number;
+    cur->next = NULL;
+
+    while (lst->next != NULL) {
+        lst = lst->next;
+    }
+
+    lst->next = cur;
+}
+
+void list_destruct(list lst)
+{
+    link cur;
+
+    while (lst != NULL) {
+        cur = lst;
+        lst = lst->next;
+        free(cur);
+    }
+}
+
 int main(void)
 {
-    list lst;
+    list lst = list_create(5);
 
-    lst = list_create(5);
+    list_insert_end(3, lst);
+    list_insert_end(6, lst);
+    list_insert_end(0, lst);
+    list_insert_end(1, lst);
+
     list_print(lst);
+    list_destruct(lst);
 
     return 0;
 }
