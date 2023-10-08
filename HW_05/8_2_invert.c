fe6a75f118d3e3787ad4ab5ccb71ade07fc1e33a
Sotvictory <sotvictory@gmail.com>
2023-10-08 22:09:48 +0300

    add 8_2 inverting integers in a binary file

---
+++ HW_05/8_2_invert.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void write_random(char *name, unsigned long int len)
+{
+    FILE *fp;
+    unsigned long int i;
+
+    if (!(fp = fopen(name, "wb"))) {
+        fprintf(stderr, "Cannot open file %s\n", name);
+        exit(1);
+    }
+
+    for (i = 0; i < len; i++) {
+        int r[1];
+        r[0] = rand() % 201 - 100;
+        fwrite(r, sizeof(int), 1, fp);
+    }
+
+    fclose(fp);
+}
+
+void exchange_raw(FILE *fp, unsigned long int p1, unsigned long int p2)
+{
+    int tmp, num1, num2;
+
+    fseek(fp, p1 * sizeof(int), SEEK_SET);
+    fread(&num1, sizeof(int), 1, fp);
+    tmp = num1;
+    fseek(fp, p2 * sizeof(int), SEEK_SET);
+    fread(&num2, sizeof(int), 1, fp);
+    fseek(fp, p1 * sizeof(int), SEEK_SET);
+    fwrite(&num2, sizeof(int), 1, fp);
+    fseek(fp, p2 * sizeof(int), SEEK_SET);
+    fwrite(&tmp, sizeof(int), 1, fp);
+}
+
+void invert_raw(FILE *fp)
+{
+    unsigned long int len = 0, cnt = 0, i = 0;
+
+    fseek(fp, 0, SEEK_END);
+    len = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    cnt = len / sizeof(int);  
+
+    for (i = 0; i < cnt / 2; i++)
+        exchange_raw(fp, i, cnt - i - 1);
+}
+
+int main(int argc, char **argv)
+{
+    FILE *fp;
+
+    if (argc != 2) {
+        fprintf(stderr, "Wrong number of arguments\n");
+        exit(1);
+    }
+
+    write_random(argv[1], 6);
+    getchar(); // pause
+
+    if (!(fp = fopen(argv[1], "rb+"))) {
+        fprintf(stderr, "Cannot open file %s\n", argv[1]);
+        exit(1);
+    }
+
+    invert_raw(fp);
+    fclose(fp);
+
+    return 0;
+}
---
+++ HW_05/README.md
@@ -0,0 +1,4 @@
+Usage of 8_2_invert: `program_name binary_file_name` <br>
+View the contents of the binary file: `od -i binary_file_name`
+
+There is a pause in the program so that you can look at the generated file and then the inverted one and compare them
