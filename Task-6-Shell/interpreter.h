4a3701d4db32f271c32a7eda7f333789cb688bbe
sotvictory <sotvictory@gmail.com>
2024-07-17 19:46:22 +0300

    add Shell

---
+++ Task-6-Shell/interpreter.h
@@ -0,0 +1,23 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include "parser.h"
+
+#define INTERPRETER_ERR -1
+
+typedef struct process *process_list;
+
+typedef struct process {
+    pid_t pid;
+    int cnt;
+    process_list next;
+    char *name;
+} process;
+
+void execute(list *lst, int *size_lst, tree cmd, process_list *zombie_list);
+void clear_zombie(process_list zombie_list, int level);
+process_list wait_zombies(process_list zombie_list);
+process_list clear_process_list(process_list list);
+void clear_resources(list *lst, int *size_lst, tree cmd);
+
+#endif /* INTERPRETER_H */
